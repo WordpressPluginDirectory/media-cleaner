03831aa089814809dd7f122e35a854de2f1f853b
Justin Gardner <justin@rhynorater.com>
2024-06-05 06:04:59 -0500

    6.7.6

--- media-cleaner/classes/rest.php
+++ media-cleaner/classes/rest.php
@@ -143,12 +143,12 @@ class Meow_WPMC_Rest
 			register_rest_route( $this->namespace, '/refresh_logs', array(
 				'methods' => 'POST',
 				'permission_callback' => array( $this->core, 'can_access_features' ),
-				'callback' => array( $this, 'refresh_logs' )
+				'callback' => array( $this, 'rest_refresh_logs' )
 			) );
 			register_rest_route( $this->namespace, '/clear_logs', array(
 				'methods' => 'POST',
 				'permission_callback' => array( $this->core, 'can_access_features' ),
-				'callback' => array( $this, 'clear_logs' )
+				'callback' => array( $this, 'rest_clear_logs' )
 			) );
 		} 
 		catch (Exception $e) {
@@ -375,21 +375,12 @@ class Meow_WPMC_Rest
 		], 200 );
 	}
 
-	function refresh_logs() {
-		$data = "No data.";
-
-		$log_file = $this->core->get_logs_path();
-
-		if ( file_exists( $log_file ) ) {
-			$data = file_get_contents( $log_file );
-		}
-
-		return new WP_REST_Response( [ 'success' => true, 'data' => $data ], 200 );
+	function rest_refresh_logs() {
+		return new WP_REST_Response( [ 'success' => true, 'data' => $this->core->get_logs() ], 200 );
 	}
 
-	function clear_logs() {
-		$log_file = $this->core->get_logs_path();
-		unlink( $log_file );
+	function rest_clear_logs() {
+		$this->core->clear_logs();
 		return new WP_REST_Response( [ 'success' => true ], 200 );
 	}
 
@@ -479,6 +470,42 @@ class Meow_WPMC_Rest
 				)
 			);
 		}
+
+		foreach ( $entries as $entry ) {
+			//Try and get a Post Title
+			$originType = $entry->originType;
+			preg_match( '/\[(.*?)\]/', $originType, $matches );
+			if ( isset( $matches[1] ) && is_numeric( $matches[1] ) ){
+				$id = $matches[1];
+				$post_title = get_the_title( $id );
+				if( $post_title ) {
+					$entry->post_title = $post_title;
+				}
+			}
+
+			// Try and get a Media URL (thumbnail)
+			$mediaId = $entry->mediaId;
+			if ( $mediaId ) {
+				$media = wp_get_attachment_image_src( $mediaId, 'thumbnail' );
+				if ( $media ) {
+					$entry->thumbnail = $media[0];
+				}
+			}
+
+			// Same but from MediaUrl if we didn't get one yet
+			$mediaUrl = $entry->mediaUrl;
+			if( $mediaUrl && empty( $entry->thumbnail ) ) {
+				// Get the ID of the attachment from its URL
+				$attachmentId = attachment_url_to_postid( $mediaUrl );
+
+				// Get the thumbnail of the attachment
+				$media = wp_get_attachment_image_src( $attachmentId, 'thumbnail' );
+				if ( $media ) {
+					$entry->thumbnail = $media[0];
+				}
+			}
+		}
+
 
 		return new WP_REST_Response( [ 'success' => true, 'data' => $entries, 'total' => $total ], 200 );
 	}
